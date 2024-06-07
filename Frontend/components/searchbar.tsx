8673768c5ddde93d471cd22b827d666af281cc9b
JuliaOberhauser <joberhau@hm.edu>
2024-06-07 13:17:42 +0200

    chore: Update file overview components and searchbar components

--- Frontend/components/searchbar.tsx
+++ Frontend/components/searchbar.tsx
@@ -1,10 +1,41 @@
-import { StyleSheet, Text } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
-export default function Search () {
-  return (
-    <Text>Componente File Overview</Text>
-  );
+export default function Search() {
+    const [searchQuery, setSearchQuery] = React.useState('');
+
+    return (
+    <View>
+        <Searchbar
+            placeholder="Suchen"
+            onChangeText={setSearchQuery}
+            value={searchQuery}
+            style={styles.searchbar}
+            iconColor='#2B4B5180'
+            placeholderTextColor={'#2B4B5180'}
+            inputStyle={styles.searchbar_input}
+        />
+    </View>
+    );
 }
 
 const styles = StyleSheet.create({
+
+    searchbar: {
+        backgroundColor: '#EDF4F3',
+        height: 38,
+        borderRadius: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        marginRight: 8,
+    },
+
+    searchbar_input: {
+        fontSize: 12,
+        fontFamily: 'Montserrat',
+        fontWeight: 'regular',
+        color: '#2B4B51',
+    },
 });
