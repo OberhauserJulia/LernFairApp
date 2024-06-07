133da26a3b0f796f754970115d75fba0fcdbf339
JuliaOberhauser <joberhau@hm.edu>
2024-06-07 13:07:40 +0200

    chore: Update file overview screens

--- Frontend/screens/file_overview_chat.tsx
+++ Frontend/screens/file_overview_chat.tsx
@@ -1,15 +1,45 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
 // import components
 import Search from '../components/searchbar';
-import Filter from '../components/filter';
 import FileOverview from '../components/file_overview';
 
-import { StyleSheet, Text } from 'react-native';
+export default function File_Overview_Chat() {
 
-export default function File_Overview_Chat () {
   return (
-    <Text>Screen File Overview Chat</Text>
+    <View style={styles.screen}>
+      <View style={styles.top_bar}>
+      </View>
+
+      <View style={styles.content}>
+        <View>
+          <Search></Search>
+        </View>
+
+        <FileOverview></FileOverview>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: '#F5F5F5',
+    flex: 1,
+  },
+
+  top_bar: {
+    height: 48,
+    backgroundColor: '#2B4B51',
+    borderBottomLeftRadius: 12,
+    borderBottomRightRadius: 12,
+  },
+
+  content: {
+    width: '100%',
+    paddingRight: 16,
+    paddingLeft: 16,
+    paddingTop: 16,
+  },
 });
