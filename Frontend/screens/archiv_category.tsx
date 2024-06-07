24eb180157bf4cd9d8e08d4388de0de17d6df5a0
JuliaOberhauser <joberhau@hm.edu>
2024-06-07 12:34:59 +0200

    chore: Update Archiv_Category screen

--- Frontend/screens/archiv_category.tsx
+++ Frontend/screens/archiv_category.tsx
@@ -1,15 +1,53 @@
-import { StyleSheet, Text } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
 
-// import Components
+// import components
 import Search from '../components/searchbar';
 import Filter from '../components/filter';
 import FileOverview from '../components/file_overview';
 
-export default function Archiv_Caregory () {
+export default function Archiv_Caregory() {
+
   return (
-    <Text>Screen Archiv Caregory</Text>
+    <View style={styles.screen}>
+      <View style={styles.top_bar}>
+      </View>
+
+      <View style={styles.content}>
+        <View style={styles.bar}>
+          <Search></Search>
+          <Filter></Filter>
+        </View>
+
+        <FileOverview></FileOverview>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: '#F5F5F5',
+    flex: 1,
+  },
+
+  top_bar: {
+    height: 48,
+    backgroundColor: '#2B4B51',
+    borderBottomLeftRadius: 12,
+    borderBottomRightRadius: 12,
+  },
+
+  content: {
+    width: '100%',
+    paddingRight: 16,
+    paddingLeft: 16,
+    paddingTop: 16,
+  },
+
+  bar: {
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
+  },
 });
