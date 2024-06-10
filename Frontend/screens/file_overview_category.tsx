a5119287e295458f5a553e07c99a0ad441052f93
Elias Eggenberger <elias.eggenberger@hm.edu>
2024-06-10 18:40:32 +0200

    dile_overview_category an backend angebunden

--- Frontend/screens/file_overview_category.tsx
+++ Frontend/screens/file_overview_category.tsx
@@ -1,13 +1,46 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 // import components
 import Search from '../components/searchbar';
 import Filter from '../components/filter';
 import FileOverview from '../components/file_overview';
 
-export default function File_Overview_Category() {
+interface File {
+  _id: { $oid: string };
+  file_id: string;
+  name: string;
+  topic?: string;
+  Fach?: string;
+  documentname: string;
+}
+
+interface File_Overview_CategoryProps {
+  subjectname: string;
+  studentname: string;
+}
+
+export default function File_Overview_Category({ subjectname, studentname }: File_Overview_CategoryProps) {
   const [searchQuery, setSearchQuery] = React.useState('');
+  const [studentName, setStudentName] = useState<string>(studentname);
+  const [files, setFiles] = useState<File[]>([]);
+  const subjectName: string = subjectname;
+
+  const getSubjectEntries = async () => {
+    try {
+      const response = await axios.get(`http://172.27.144.1:8000/getfiles/${studentName}/${subjectName}`);
+      setFiles(response.data.files);
+      console.log(files)
+    } catch (error) {
+      console.error("Error fetching subject entries:", error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("Loading");
+    getSubjectEntries();
+  }, []); // Abhängigkeiten-Array, um sicherzustellen, dass es nur einmal aufgerufen wird
 
   return (
     <View style={styles.screen}>
@@ -16,11 +49,20 @@ export default function File_Overview_Category() {
 
       <View style={styles.content}>
         <View style={styles.bar}>
-          <Search></Search>
-          <Filter></Filter>
+          <Search />
+          <Filter />
         </View>
-
-        <FileOverview></FileOverview>
+        {files.map(file => (
+          <FileOverview
+            key={file._id.$oid}
+            _id={file._id.$oid}
+            file_id={file.file_id}
+            topic={file.topic || 'Unknown Topic'}
+            subject={file.Fach || 'Unknown Subject'}
+            dateiname={file.documentname}
+            filename={file.name}
+          />
+        ))}
       </View>
     </View>
   );
