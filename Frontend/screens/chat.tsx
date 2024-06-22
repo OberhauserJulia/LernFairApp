55c141020b1e985ac015268b89a792120dfeabad
Tatjana Y <t.yauseyenka@hm.edu>
2024-06-22 04:49:59 +0200

    Made my Chat.screen no implemntation yet

---
+++ Frontend/screens/chat.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.center}>
+        <Text style={styles.text}>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
